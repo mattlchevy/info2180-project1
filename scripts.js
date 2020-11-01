45ef993c3f94b750d7c490f72dde240a2fe6b02a
mattlchevy <lear.chevy@hotmail.com>
2020-11-01 01:45:25 -0500

    fonts, scripts progress

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
 /* Add your JavaScript to this file */
+window.onload = () =>{
+    const btn = document.getElementsByClassName("btn");
+
+    btn.addEventListener('submit', function(event){
+        event.preventDefault();
+
+        var emailadd = getElementsByID("email").value;
+
+        console.log(emailadd);
+
+    })
+}
+
