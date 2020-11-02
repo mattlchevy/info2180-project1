4e572f728ee7541b294f4e9b94088ed899e19ab1
mattlchevy <lear.chevy@hotmail.com>
2020-11-01 22:20:02 -0500

    subscription

--- scripts.js
+++ scripts.js
@@ -1,14 +1,19 @@
 /* Add your JavaScript to this file */
-window.onload = () =>{
-    const btn = document.getElementsByClassName("btn");
+"use strict";
 
-    btn.addEventListener('submit', function(event){
-        event.preventDefault();
 
-        var emailadd = getElementsByID("email").value;
+var subscribe = function(){
 
-        console.log(emailadd);
+    document.getElementsByClassName('btn')[1].addEventListener('click',function(e){
+        e.preventDefault();
+        var email = document.getElementById('email').value;
+        if (email === ''){
+            document.getElementsByClassName('message')[0].innerHTML = 'Please enter a valid email address.';
+        }else{
+            document.getElementsByClassName('message')[0].innerHTML = 'Thank you! Your email address ' + email + 'has been added to the mailing list!';
+        }
+        return false;   
+    });
 
-    })
 }
-
+window.onload = subscribe;
